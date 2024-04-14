b8eb3815e05fe13f01769c24cd630b8732145013
Rhys Johns <rhysjohnsdev@gmail.com>
2024-04-14 21:23:49 +1000

    Accept file location so we don't need to embed files

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
+import { parseArgs } from "https://deno.land/std@0.207.0/cli/parse_args.ts";
+
 /*
  *  Author: Rhys Johns
  *
  *  This is honestly just a super basic program - we load up a list of quotes,
  *  then print one at random to the console. That's it really!
  */
-function main(): void {
-  const fileLocation = "./quotes.txt";
+function main(fileLocation: string): void {
   const rawQuotes: string = new TextDecoder().decode(
     Deno.readFileSync(fileLocation),
   );
@@ -15,5 +16,10 @@ function main(): void {
   console.log(quotes[quoteIndex]);
 }
 
+// This is pretty neat! Based on minimistjs
+const args = parseArgs(Deno.args, {
+  string: ["location"],
+});
+
 // Run the main program
-main();
+main(args.location ?? "quotes.txt");
